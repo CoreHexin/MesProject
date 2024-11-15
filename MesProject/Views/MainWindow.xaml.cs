11fdd129287a06fc110975ad58fdf2f1692758cc
Hexin <hexin.email@qq.com>
2024-11-15 10:36:03 +0800

    MainWindow移入Views文件夹

--- MesProject/Views/MainWindow.xaml.cs
+++ MesProject/Views/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MesProject
+namespace MesProject.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
