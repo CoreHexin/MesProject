89ed1076284125e16b1b196974220c38fae7550a
Hexin <hexin.email@qq.com>
2024-11-18 16:56:09 +0800

    环境数据显示

---
+++ MesProject/Models/EnvironmentModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MesProject.Models
+{
+    public class EnvironmentModel
+    {
+        public string Name { get; set; }
+        public double Value { get; set; }
+    }
+}
--- MesProject/ViewModels/MainWindowViewModel.cs
+++ MesProject/ViewModels/MainWindowViewModel.cs
@@ -1,105 +1,120 @@
-﻿namespace MesProject.ViewModels
+﻿using MesProject.Models;
+
+namespace MesProject.ViewModels
 {
     public class MainWindowViewModel : NotificationObject
     {
-		private string _timeStr = string.Empty;
+        private string _timeStr = string.Empty;
 
-		public string TimeStr
-		{
-			get { return _timeStr; }
-			set 
-			{ 
-				_timeStr = value;
-				RaisePropertyChanged();
-			}
-		}
+        public string TimeStr
+        {
+            get { return _timeStr; }
+            set
+            {
+                _timeStr = value;
+                RaisePropertyChanged();
+            }
+        }
 
-		private string _dateStr = string.Empty;
+        private string _dateStr = string.Empty;
 
-		public string DateStr
-		{
-			get { return _dateStr; }
-			set 
-			{ 
-				_dateStr = value; 
-				RaisePropertyChanged();
-			}
-		}
+        public string DateStr
+        {
+            get { return _dateStr; }
+            set
+            {
+                _dateStr = value;
+                RaisePropertyChanged();
+            }
+        }
 
-		private string _weekStr = string.Empty;
+        private string _weekStr = string.Empty;
 
-		public string WeekStr
-		{
-			get { return _weekStr; }
-			set 
-			{ 
-				_weekStr = value;
-				RaisePropertyChanged();
-			}
-		}
+        public string WeekStr
+        {
+            get { return _weekStr; }
+            set
+            {
+                _weekStr = value;
+                RaisePropertyChanged();
+            }
+        }
 
 
-		private string _machineCount;
+        private string _machineCount;
 
         // 机台总数
         public string MachineCount
-		{
-			get { return _machineCount; }
-			set 
-			{ 
-				_machineCount = value;
-				RaisePropertyChanged();
-			}
-		}
-
-
-		private string _productCount;
-
-		// 生产计数
-		public string ProductCount
-		{
-			get { return _productCount; }
-			set 
-			{ 
-				_productCount = value; 
-				RaisePropertyChanged();
-			}
-		}
-
-
-		private string _unqualifiedCount;
-
-		// 不良计数
-		public string UnqualifiedCount
         {
-			get { return _unqualifiedCount; }
-			set 
-			{ 
-				_unqualifiedCount = value; 
-				RaisePropertyChanged();
-			}
-		}
-
-		public MainWindowViewModel()
-        {
-            UpdateTime();
-			_machineCount = "298";
-			_productCount = "1643";
-			_unqualifiedCount = "34";
+            get { return _machineCount; }
+            set
+            {
+                _machineCount = value;
+                RaisePropertyChanged();
+            }
         }
 
-		private void UpdateTime()
-		{
-			TimeStr = DateTime.Now.ToString("HH:mm");
-			DateStr = DateTime.Now.ToString("yyyy-MM-dd");
-			WeekStr = GetChineseWeek();
-		}
 
-		private string GetChineseWeek()
-		{
-			int weekIndex = (int)DateTime.Now.DayOfWeek;
-			string[] weeks = new string[7] { "星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六" };
-			return weeks[weekIndex];
-		}
+        private string _productCount;
+
+        // 生产计数
+        public string ProductCount
+        {
+            get { return _productCount; }
+            set
+            {
+                _productCount = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+        private string _unqualifiedCount;
+
+        // 不良计数
+        public string UnqualifiedCount
+        {
+            get { return _unqualifiedCount; }
+            set
+            {
+                _unqualifiedCount = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public List<EnvironmentModel> EnvironmentModels { get; set; }
+
+        public MainWindowViewModel()
+        {
+            UpdateTime();
+            _machineCount = "298";
+            _productCount = "1643";
+            _unqualifiedCount = "34";
+
+            EnvironmentModels = new List<EnvironmentModel>()
+            {
+                new EnvironmentModel() { Name = "光照(Lux)", Value = 123},
+                new EnvironmentModel() { Name = "噪音(db)", Value = 55},
+                new EnvironmentModel() { Name = "温度(℃)", Value = 80},
+                new EnvironmentModel() { Name = "湿度(%)", Value = 43},
+                new EnvironmentModel() { Name = "PM2.5", Value = 20},
+                new EnvironmentModel() { Name = "硫化氢(PPM)", Value = 15},
+                new EnvironmentModel() { Name = "氮气(PPM)", Value = 18},
+            };
+        }
+
+        private void UpdateTime()
+        {
+            TimeStr = DateTime.Now.ToString("HH:mm");
+            DateStr = DateTime.Now.ToString("yyyy-MM-dd");
+            WeekStr = GetChineseWeek();
+        }
+
+        private string GetChineseWeek()
+        {
+            int weekIndex = (int)DateTime.Now.DayOfWeek;
+            string[] weeks = new string[7] { "星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六" };
+            return weeks[weekIndex];
+        }
     }
 }
