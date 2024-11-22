51ee1e65dda3c19eb7d3f7833ccd34862b84755b
Hexin <hexin.email@qq.com>
2024-11-22 17:28:38 +0800

    添加动画附加属性

---
+++ MesProject/AttachedProperties/AnimationAttachedProperties.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using MesProject.Animation;
+
+namespace MesProject.AttachedProperties
+{
+    public class AnimationAttachedProperties
+    {
+        public static bool GetFadeInAndSlideFromBottom(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(FadeInAndSlideFromBottomProperty);
+        }
+
+        public static void SetFadeInAndSlideFromBottom(DependencyObject obj, bool value)
+        {
+            obj.SetValue(FadeInAndSlideFromBottomProperty, value);
+        }
+
+        public static readonly DependencyProperty FadeInAndSlideFromBottomProperty =
+            DependencyProperty.RegisterAttached(
+                "FadeInAndSlideFromBottom",
+                typeof(bool),
+                typeof(AnimationAttachedProperties),
+                new PropertyMetadata(OnFadeInAndSlideFromBottomPropertyChanged)
+            );
+
+        private static void OnFadeInAndSlideFromBottomPropertyChanged(
+            DependencyObject d,
+            DependencyPropertyChangedEventArgs e
+        )
+        {
+            if (!(d is FrameworkElement element))
+                return;
+
+            bool value = (bool)e.NewValue;
+            if (value)
+            {
+                element.FadeInAndSlideFromBottom(0.3f);
+            }
+        }
+    }
+}
