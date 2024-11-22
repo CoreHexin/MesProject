63bfd56f4d6a2945ecbc1e3b17ed1c8677e373cc
Hexin <hexin.email@qq.com>
2024-11-22 17:28:19 +0800

    添加动画

---
+++ MesProject/Animation/ElementAnimations.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace MesProject.Animation
+{
+    public static class ElementAnimations
+    {
+        public static void FadeInAndSlideFromBottom(this FrameworkElement element, float seconds)
+        {
+            var storyboard = new Storyboard();
+            storyboard.AddFadeIn(seconds);
+            storyboard.AddSlideFromBottom(seconds, element.ActualHeight);
+            storyboard.Begin(element);
+        }
+    }
+}
