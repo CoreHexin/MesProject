8240bf8466a7b0dfef6d1932db7d907615ed24c0
Hexin <hexin.email@qq.com>
2024-11-25 20:52:26 +0800

    机台详情页设计

---
+++ MesProject/ViewModels/WorkshopDetailViewModel.cs
@@ -0,0 +1,44 @@
+﻿using MesProject.Models;
+
+namespace MesProject.ViewModels
+{
+    public class WorkshopDetailViewModel : NotificationObject
+    {
+        private List<MachineModel> _machineModels;
+
+        public List<MachineModel> MachineModels
+        {
+            get { return _machineModels; }
+            set
+            {
+                _machineModels = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public WorkshopDetailViewModel()
+        {
+            _machineModels = new List<MachineModel>();
+            GenerateMachineModels();
+        }
+
+        private void GenerateMachineModels()
+        {
+            var random = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                int planNum = random.Next(100, 1000);
+                MachineModels.Add(
+                    new MachineModel()
+                    {
+                        Name = $"焊接机-{i + 1}",
+                        PlanNum = planNum,
+                        CompleteNum = random.Next(0, planNum),
+                        Status = "作业中",
+                        OrderNo = $"H00100-{i + 1}",
+                    }
+                );
+            }
+        }
+    }
+}
